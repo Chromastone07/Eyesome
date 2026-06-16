1ab6aea40702020958c3ffccc4578d26ac98c6e3
chromastone07 <okappeal@gmail.com>
2026-06-16 13:47:10 +0530

    security

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const cspHeader = {
   key: 'Content-Security-Policy',
   value: [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-eval'",
+    "script-src 'self' 'unsafe-inline'",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
     "font-src 'self' https://fonts.gstatic.com",
     "img-src 'self' data:",
